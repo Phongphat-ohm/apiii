6997634eb04062b0d83731eab7d3c130600504e7
Phongphat <130993206+Phongphat-ohm@users.noreply.github.com>
2023-07-22 16:58:09 +0700

    ติดคอร์ตาย

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
